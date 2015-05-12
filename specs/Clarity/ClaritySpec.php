3af2f73524b0c07b22c1a90364ea03967c3278a3
bound1ess <bound1ess.dev@gmail.com>
2015-05-12 21:02:23 +0600

    Configure PhpSpec.

---
+++ specs/Clarity/ClaritySpec.php
@@ -0,0 +1,11 @@
+<?php namespace specs\Clarity;
+
+use PhpSpec\ObjectBehavior;
+
+class ClaritySpec extends ObjectBehavior {
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Clarity\Clarity');
+    }
+}
---
+++ src/Clarity/Clarity.php
@@ -0,0 +1,6 @@
+<?php namespace Clarity;
+
+class Clarity {
+
+    // ...
+}
